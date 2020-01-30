26a081f6b947fd8575e19f21fa97714fddcc05a6
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-01-30 16:20:59 +0000

    adding updated docker run support

--- docker_build_index_pages.sh
+++ docker_build_index_pages.sh
@@ -1,17 +1,17 @@
 # build docker
-DOCKER_PATH="./Dockerfile_index"
+DOCKER_PATH="./Dockerfile_index_pages"
 
 echo "Trying to build docker image from:" $DOCKER_PATH;
 
 if [ -f "$DOCKER_PATH" ]; then
   echo "file exits"
-  sudo docker build -t iainmackie/galago-index -f $DOCKER_PATH .
+  sudo docker build -t iainmackie/galago-index-pages -f $DOCKER_PATH .
   sudo docker run -it \
   	-v $PWD/shared/data/:/home/shared/data \
   	-v $PWD/shared/index/:/home/shared/index \
-  	-e INPUT=/home/shared/data/paragraphs/dedup.articles-paragraphs.cbor \
+  	-e INPUT=/home/shared/data/pages/unprocessedAllButBenchmark.Y2.cbor \
     -e INDEX=/home/shared/index/galago-index \
-  	iainmackie/galago-index
+  	iainmackie/galago-index-pages
 
 else
   echo "Error - path to file not found:" $DOCKER_PATH;
---
+++ docker_build_index_paragraphs.sh
@@ -0,0 +1,20 @@
+# build docker
+DOCKER_PATH="./Dockerfile_index_paragraphs"
+
+echo "Trying to build docker image from:" $DOCKER_PATH;
+
+if [ -f "$DOCKER_PATH" ]; then
+  echo "file exits"
+  sudo docker build -t iainmackie/galago-index-paragraphs -f $DOCKER_PATH .
+  sudo docker run -it \
+  	-v $PWD/shared/data/:/home/shared/data \
+  	-v $PWD/shared/index/:/home/shared/index \
+  	-e INPUT=/home/shared/data/paragraphs/dedup.articles-paragraphs.cbor \
+    -e INDEX=/home/shared/index/galago-index \
+  	iainmackie/galago-index-paragraphs
+
+else
+  echo "Error - path to file not found:" $DOCKER_PATH;
+  exit 0;
+
+fi
